9f908014513fad970c31d2fda34574bb7e917b05
TigerZhu10 <143968664+TigerZhu10@users.noreply.github.com>
2023-09-17 10:42:10 +0800

    code clean

--- src/5641AS.cpp
+++ src/5641AS.cpp
@@ -17,6 +17,12 @@ int pos_control [4] = {d1, d2, d3, d4};
 
 int Segment [7] = {a,b,c,d,e,f,g};
 
+int digits[4] = {0,0,0,0};
+int first_number = 0;
+int second_number = 0;
+int third_number = 0;
+int fourth_number = 0;
+int display_digit [4] = {first_number, second_number, third_number, fourth_number};
 
 
 int number [10][7] = {
@@ -85,27 +91,18 @@ void LED_Init(void) {
         digitalWrite(pos_control[i], HIGH);
     }
 }
+
 /*
 OBJECTIVE: make a function that we can transfer it if we need it
 PARMETER: void
 NOTE: The minute(); and the second(); is for us to light up the number 1-9. Because we have to call them so we have to write a name for it like digits[0]
-
 */
-
 void Time_set(){
     digits[0] = minute() / 10;
     digits[1] = minute() % 10;
     digits[2] = second() / 10;
     digits[3] = second() % 10;
 }
-
-int digits[4] = {0,0,0,0};
-int first_number = 0;
-int second_number = 0;
-int third_number = 0;
-int fourth_number = 0;
-int display_digit [4] = {first_number, second_number, third_number, fourth_number};
-
 
 
 /*
@@ -116,53 +113,14 @@ NOTE: void(for now)
 void Display_Realtime(){
 
 
-        // int fourth_number = second();
-        // int third_number = second();
-        // int second_number = minute();
-        // int first_number = minute();
+      Time_set();
 
-
-        Time_set();
       for(int i = 0; i < 4; i++){
         
         DisplaySingle(i,digits[i]);
         Led_clear();
         digitalWrite(pos_control[i],HIGH);
       }
-       
-
-
-     //  int fourth_number = second();
-     //  fourth_number = fourth_number % 10;
-    //   DisplaySingle(3,fourth_number);
-    //   Led_clear();
-    //   digitalWrite(pos_control[3], HIGH);
-    //   int third_number = second();
-    //   third_number = third_number/10;
-    //   DisplaySingle(2,third_number);
-    //   Led_clear();
-    //   digitalWrite(pos_control[2], HIGH);
-
-    //   int second_number = minute();
-    //   second_number = second_number % 10;
-    //   DisplaySingle(1,second_number);
-    //   Led_clear(); 
-    //   digitalWrite(pos_control[1], HIGH);
-    //   int first_number = minute(); 
-    //   first_number = first_number/10;
-    //   DisplaySingle(0,first_number);
-    //   Led_clear();
-    //   digitalWrite(pos_control[0], HIGH);
-
-
-
-    //  for(int i = 0; i < 4; i++){
-    //     DisplaySingle(i,display_digit[i]);
-    //     Led_clear();
-    //     digitalWrite(pos_control[i],HIGH);
-        
-    //   }
     
-
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,14 @@
 #include "5641AS.h"
 #include <TimeLib.h>
 
+
 void setup() {
     Serial.begin(9600);
     LED_Init();
- 
 }
 
 void loop() {
- 
-    // int currentSecond = second();
-    // Serial.println(currentSecond);
-    //LED_on();
+
     Display_Realtime();
 
 }
